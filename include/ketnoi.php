aa604a77f8d288e1364dcf33fa99851320c0f383
anhtaidesu <tonthatanhtai2000@gmail.com>
2022-12-25 18:52:13 +0700

    TaiCommit

--- include/ketnoi.php
+++ include/ketnoi.php
@@ -2,6 +2,6 @@
 	$ung=mysqli_connect("localhost","root","") ;
 //@Shop99k@
 	
-	$strSQL=mysqli_select_db($ung,'cuahangcaycanh');
+	$strSQL=mysqli_select_db($ung,'cuahangquanao');
       mysqli_query($ung,"SET NAMES 'utf8'");
 ?>
